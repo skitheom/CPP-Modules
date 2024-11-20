685fda5bd27f7e483f04512f9d1de7989e81fb25
skitheom <SahoKitahara@my.uopeople.edu>
2024-11-21 01:50:08 +0900

    ex00 done

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+static void print_to_upper(const char *str) {
+
+    if (!str)
+        return;
+    for (int i = 0; str[i]; i++) {
+        std::cout << static_cast<char>(std::toupper(str[i]));
+    }
+}
+
+int main(int argc, const char **argv) {
+    
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+    for (int i = 1; i < argc; i++) {
+        print_to_upper(argv[i]);
+    }
+    std::cout << std::endl;
+    return 0;
+}
